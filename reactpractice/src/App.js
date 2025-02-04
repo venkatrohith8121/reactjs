dcc2ceadbd78b7160fd2cb72d32c931bd74ad80f
venkatrohith8121 <venkatrohithkandloori@gmail.com>
2025-02-04 23:31:21 +0530

    04/02/2025

--- reactpractice/src/App.js
+++ reactpractice/src/App.js
@@ -1,14 +1,12 @@
 import './App.css';
 import '../node_modules/bootstrap/dist/css/bootstrap.css'
 import Navbar from './components/Navbar/Navbar';
-import Login from './components/Login/Login';
 
 function App() {
   return (
 
     <div>
       <Navbar />
-      <Login/>
     </div>
   );
 }
---
+++ reactpractice/src/components/Regestration/Regestration.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Regestration = () => {
+  return (
+    <div>Regestration</div>
+  )
+}
+
+export default Regestration
